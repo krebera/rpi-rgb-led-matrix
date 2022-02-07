affe73c257a18afc9c71ae2af177ab89c7dcfe77
Keenan Rebera <krebera@umich.edu>
2022-02-07 15:11:41 -0800

    happy riding

---
+++ bindings/python/krebera/happyriding.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# Display a runtext with double-buffering.
+from samplebase import SampleBase
+from rgbmatrix import graphics
+import time
+
+
+class RunText(SampleBase):
+    def __init__(self, *args, **kwargs):
+        super(RunText, self).__init__(*args, **kwargs)
+
+    def run(self):
+        offscreen_canvas = self.matrix.CreateFrameCanvas()
+        font = graphics.Font()
+        font.LoadFont("../../../fonts/7x13.bdf")
+        textColor = graphics.Color(255, 255, 0)
+        pos = 0
+        my_text = "Happy Riding"
+
+        while True:
+            offscreen_canvas.Clear()
+            graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
+            time.sleep(0.05)
+            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+
+
+# Main function
+if __name__ == "__main__":
+    run_text = RunText()
+    if (not run_text.process()):
+        run_text.print_help()
