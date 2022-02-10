0b9d519fbafeb7954d62eaf68a21eb7f0b8411fb
Keenan Rebera <krebera@umich.edu>
2022-02-09 17:11:34 -0800

    woop

--- bindings/python/krebera/bike_runner.py
+++ bindings/python/krebera/bike_runner.py
@@ -8,16 +8,16 @@ import sys
 
 # Custom Canvases
 from animtest import LoadingBar
-from stupid import StupidTest
+from make_wooper import Wooper
 
-bar = LoadingBar()
+# bar = LoadingBar()
 
-async def updateBar():
-    progress = 0
-    while progress < 100:
-        bar.render(progress)
-        await asyncio.sleep(0.05)
-        progress = progress + 1
+# async def updateBar():
+#     progress = 0
+#     while progress < 100:
+#         bar.render(progress)
+#         await asyncio.sleep(0.05)
+#         progress = progress + 1
 
 # TODO: APPLICATION STATE MANAGER WITH ACTIVE WINDOW
 # TODO: GENERAL WINDOW CLASS WITH AN UPDATE, SETUP, TEARDOWN
@@ -27,19 +27,21 @@ async def updateBar():
 #TODO: You need to create exactly one matrix and then assign and deassign views to it
 # You can't own multiple
 
-async def main():
-    tasks = []
-    tasks.append(asyncio.ensure_future(updateBar()))
-    tasks.append(asyncio.ensure_future(get_strava_data()))
+# async def main():
+#     tasks = []
+#     tasks.append(asyncio.ensure_future(updateBar()))
+#     tasks.append(asyncio.ensure_future(get_strava_data()))
 
-    json = await asyncio.gather(*tasks)
-    print(json)
-    bar.dismiss()
+#     json = await asyncio.gather(*tasks)
+#     print(json)
+#     bar.dismiss()
 
 try:
     # Start loop
     print("Press CTRL-C to stop sample")
-    asyncio.run(main())
+    # asyncio.run(main())
+    woop = Wooper()
+    woop.render()
 except KeyboardInterrupt:
     print("Exiting\n")
     sys.exit(0)
--- bindings/python/krebera/make_wooper.py
+++ bindings/python/krebera/make_wooper.py
@@ -8,28 +8,22 @@ class Wooper(SampleBase):
     def __init__(self):
         super(Wooper, self).__init__()
 
-        # im = Image.open('./temp/pokemon.png').convert('RGBA')
-        # w, h = im.size
+        im = Image.open('./temp/pokemon.png').convert('RGBA')
+        w, h = im.size
 
         nw = 50
         nh = 50
 
-        # left = (w - nw)//2
-        # top = (h - nh)//2
-        # right = (w + nw)//2
-        # bottom = (h + nh)//2
+        left = (w - nw)//2
+        top = (h - nh)//2
+        right = (w + nw)//2
+        bottom = (h + nh)//2
 
         # Crop the center of the image
-        # im = im.crop((left, top, right, bottom))
+        im = im.crop((left, top, right, bottom))
 
-        # background = Image.new('RGBA', im.size, (0,0,0))
-        # self.pokemon = Image.alpha_composite(background, im).convert('RGB')
+        background = Image.new('RGBA', im.size, (0,0,0))
+        self.pokemon = Image.alpha_composite(background, im).convert('RGB')
 
     def render(self):
-        # w, h = self.pokemon.size
-
-        image = Image.new("RGB", (64, 64))  # Can be larger than matrix if wanted!!
-        draw = ImageDraw.Draw(image)  # Declare Draw instance before prims
-        draw.rectangle((0, 0, 56, 10), fill=(0, 0, 0), outline=(0, 255, 0))
-        self.matrix.SetImage(image, 0, 0)
-        # self.matrix.SetImage(self.pokemon, 0, 0)
+        self.matrix.SetImage(self.pokemon, 0, 0)
