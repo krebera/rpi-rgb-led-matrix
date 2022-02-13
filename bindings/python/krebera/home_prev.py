443e452aca16dc744547fdf4dd76453562693725
Keenan Rebera <krebera@umich.edu>
2022-02-13 14:40:21 -0800

    home jpg

--- bindings/python/krebera/home_prev.py
+++ bindings/python/krebera/home_prev.py
@@ -12,7 +12,7 @@ from im_utils import ImagePreview
 
 if __name__ == "__main__":
     prev = ImagePreview()
-    prev.set_im(Image.open("./assets/home.jpg"))
+    prev.set_im(Image.open("./assets/home.ppm"))
     prev.render()
     time.sleep(30)
     # canvas.show()
